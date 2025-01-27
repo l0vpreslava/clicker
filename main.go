3d2a6b6b4b45c9155da50f2531418da8dca43270
l0vpreslava <l0vpre@tutamail.com>
2025-01-27 18:20:44 +0300

    Add an effect when destroying the target.

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
-//TODO: Обработать нажатие мышки так, что если игрок нажал в пределах мишени, то эту мишень удалить из списка
 //TODO: Добавить какой-нибудь эффект при уничтожении мишени
 //TODO: Добавить счётчик очков
-//TODO: Добавить ускорение спавна мишеней со временем
 //TODO: Добавить условие проигрыша, когда мишеней станет слишком много
 //TODO: Добавить меню, в котором будет кнопка играть, настройки и выход
 //TODO: Нарисовать фоны, мишени, курсор, эффект курсора при нажатии
@@ -20,23 +18,41 @@ import (
 )
 
 type Game struct {
-    ballPosition  rl.Vector2
-    ballColor     rl.Color
-    targets       []rl.Vector2
-    targetRadius  float32
-    spawnInterval float32
-    lastSpawnTime float32
+    ballPosition   rl.Vector2
+    ballColor      rl.Color
+    targets        []rl.Vector2
+    targetRadius   float32
+    spawnInterval  float32
+    lastSpawnTime  float32
+    removedTargets []RemovedTarget
 }
 
 func NewGame() Game {
     return Game{
-        ballPosition:  rl.NewVector2(100, 100),
-        ballColor:     rl.GetColor(0xf96e61ff),
-        targets:       make([]rl.Vector2, 0),
-        targetRadius:  30.0,
-        spawnInterval: 1.5,
-        lastSpawnTime: 0,
+        ballPosition:   rl.NewVector2(100, 100),
+        ballColor:      rl.GetColor(0xf96e61ff),
+        targets:        make([]rl.Vector2, 0),
+        targetRadius:   30.0,
+        spawnInterval:  1.5,
+        lastSpawnTime:  0,
+        removedTargets: make([]RemovedTarget, 0),
     }
+}
+
+type RemovedTarget struct {
+    position rl.Vector2
+    timer    float32
+}
+
+func ShowDeleteAnimation(target RemovedTarget, dt float32) rl.Vector2 {
+    const fallSpeed = 500.0    // u
+    const acceleration = 200.0 // a
+
+    target.timer += dt // t
+    // s = ut + 1/2*at^2
+    target.position.Y += fallSpeed*target.timer + 0.5*acceleration*target.timer*target.timer
+
+    return target.position
 }
 
 const ScreenWidth = 800.0
@@ -59,8 +75,8 @@ func main() {
         game.ballPosition = rl.GetMousePosition()
 
         if timer-game.lastSpawnTime >= game.spawnInterval {
-            x := rand.Float32() * (ScreenWidth - 2.0*game.targetRadius) + game.targetRadius
-            y := rand.Float32() * (ScreenHeight - 2.0*game.targetRadius) + game.targetRadius
+            x := rand.Float32()*(ScreenWidth-2.0*game.targetRadius) + game.targetRadius
+            y := rand.Float32()*(ScreenHeight-2.0*game.targetRadius) + game.targetRadius
             game.targets = append(game.targets, rl.NewVector2(x, y))
             game.lastSpawnTime = timer
 
@@ -69,18 +85,31 @@ func main() {
             }
         }
 
-
         if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
             targets := make([]rl.Vector2, len(game.targets))
             index := 0
             for _, target := range game.targets {
-                if !rl.CheckCollisionPointCircle(rl.GetMousePosition(),target, game.targetRadius-10){
+                if !rl.CheckCollisionPointCircle(rl.GetMousePosition(), target, game.targetRadius-10) {
                     targets[index] = target
                     index++
+                } else {
+                    game.removedTargets = append(game.removedTargets, RemovedTarget{position: target, timer: 0})
                 }
             }
             game.targets = targets[:index]
         }
+
+        for i := range game.removedTargets {
+            game.removedTargets[i].position = ShowDeleteAnimation(game.removedTargets[i], dt)
+        }
+
+        newRemovedTargets := make([]RemovedTarget, 0)
+        for _, removedTarget := range game.removedTargets {
+            if removedTarget.position.Y < ScreenHeight {
+                newRemovedTargets = append(newRemovedTargets, removedTarget)
+            }
+        }
+        game.removedTargets = newRemovedTargets
 
         rl.BeginDrawing()
 
@@ -90,11 +119,11 @@ func main() {
         for _, target := range game.targets {
             rl.DrawCircleV(target, float32(game.targetRadius-10), rl.GetColor(0x9f80fcff))
         }
+
+        for _, removedTarget := range game.removedTargets {
+            rl.DrawCircleV(removedTarget.position, float32(game.targetRadius-10), rl.GetColor(0x9f80fcff))
+        }
+
         rl.EndDrawing()
     }
 }
-
-
-
-
-
